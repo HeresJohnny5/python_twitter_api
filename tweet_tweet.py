3fdf5f51ab1c9feb091a6695b0270b6add42417a
HeresJohnny5 <ericksonjohn01@hotmail.com>
2020-01-14 22:21:01 -0500

    Add helper method to limit the amount of API calls when reviewing followers.

--- tweet_tweet.py
+++ tweet_tweet.py
@@ -1,10 +1,21 @@
 import tweepy
+import time
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+
+def limit_handler(cursor):
+    try:
+        while True:
+            yield cursor.next()
+    except tweepy.RateLimitError:
+        time.sleep(1000)
+
+
+for follower in limit_handler(tweepy.Cursor(api.followers).items()):
+    if follower.name == 'Follower Name':
+        follower.unfollow()
+        break
